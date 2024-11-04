d6c692adfc6e9c23a649f976258c1ac658386ff2
DELFIN <delfin000008@gmail.com>
2024-11-04 14:25:11 +0500

    Создана функция exchange для получения из интернета курсов криптовалют

--- ObmenCryptocurrencies.py
+++ ObmenCryptocurrencies.py
@@ -1,9 +1,24 @@
-from idlelib.pyshell import extended_linecache_checkcache
-
 import requests
 from tkinter import *
 from tkinter import messagebox as mb
 
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get("https://api.coingecko.com/api/v3/exchange_rates")
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rete = data["rates"][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rete} за 1 крипту")
+            else:
+                mb.showerror("Ошибка!", f"Криптавалюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", f"Введите код криптовалюты")
 
 window = Tk()
 window.title("Курс обмена криптовалют")
@@ -14,7 +29,7 @@ Label(text="Введите код криптовалюты").pack(padx=10, pady=
 entry = Entry()
 entry.pack(padx=10, pady=10)
 
-Button(text="Получить курс обмена", command=extended).pack(padx=10, pady=10)
+Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
 
 window.mainloop()
 #url = "https://api.coingecko.com/api/v3/exchange_rates"
